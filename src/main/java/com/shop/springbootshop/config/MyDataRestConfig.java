fb25853bc6676da008b03803f5ef8d634bde8034
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2022-10-09 18:15:26 +0200

    Disabled POST, DELETE and PUT

---
+++ src/main/java/com/shop/springbootshop/config/MyDataRestConfig.java
@@ -0,0 +1,30 @@
+package com.shop.springbootshop.config;
+
+import com.shop.springbootshop.entity.Product;
+import com.shop.springbootshop.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+
+@Configuration // So spring can pick this up
+public class MyDataRestConfig implements RepositoryRestConfigurer, MyDataRestConfig1 {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+
+        // Disable PUT, POST and DELETE for product
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+        // Disable PUT, POST and DELETE for ProductCategory
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+    }
+}
---
+++ src/main/java/com/shop/springbootshop/config/MyDataRestConfig1.java
@@ -0,0 +1,7 @@
+package com.shop.springbootshop.config;
+
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+
+public interface MyDataRestConfig1 {
+    void configureRepositoryRestConfiguration(RepositoryRestConfiguration config);
+}
