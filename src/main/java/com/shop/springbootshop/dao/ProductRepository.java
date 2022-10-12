591d17b5f9c11078b95e6761ecd7e4a55215b7a1
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2022-10-12 11:42:36 +0200

    Added CORS support for the Angular app. Port 4200

--- src/main/java/com/shop/springbootshop/dao/ProductRepository.java
+++ src/main/java/com/shop/springbootshop/dao/ProductRepository.java
@@ -2,6 +2,8 @@ package com.shop.springbootshop.dao;
 
 import com.shop.springbootshop.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200") // accept calls from web browser apps from this origin
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
